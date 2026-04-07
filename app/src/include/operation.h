a2c34285eb6e013f9696cc52fd7e6a3576da3288
eitak <eitakuboki@gmail.com>
2026-04-06 18:26:56 -0700

    threaded version of operation

--- app/src/include/operation.h
+++ app/src/include/operation.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #include "set.h"
 #include "hal/bts7960.h"
@@ -13,22 +15,28 @@
 * Operation Mode Header
 *
 * Operations within Operation Mode:
-* 1. Launch the ball based on entries in the set seuqence
+* 1. Launch the ball based on entries in the set sequence
 * 2. User can stop the machine at any time
 * 3. User can repeat the same set
 * 4. User can shuffle the set sequence
 */
+extern float curr_tilt_angle;
+extern float curr_yaw_angle;
 
-void operation_init();
-void operation_cleanup();
+tb6600_t motor;
 
+//communicates with the hal files to send signal to the machine
 void tilt_signal(float angle);
 void yaw_signal(float angle);
 void speed_signal(float speed);
+
 void set_machine(int set_index);
-void machine_operating();
+
+void operation_init();
 void stop_machine();
 void repeat_set();
 void shuffle_set_sequence();
+
+void machine_operating();
 
 #endif // OPERATION_H
